286c43d59f6f56b286188664f2c2df9158470920
Enbi81 <wotng2002@gmail.com>
2024-06-06 01:22:48 +0200

    V1.2.9 - Fixing color palette rotation

--- src/hooks/useGlobalColorPalette.ts
+++ src/hooks/useGlobalColorPalette.ts
@@ -3,7 +3,7 @@ import { LightColorPalettes } from '../colorPalettes';
 export const useGlobalColorPalette = () => {
   const today = new Date();
   const colorPalettes = LightColorPalettes;
-  const prime = 136614254054256523129288861637; // large enough to be pseudo random
+  const prime = 7919;
 
   let colorPaletteNumber = (today.getDate() * prime) % colorPalettes.length;
 
