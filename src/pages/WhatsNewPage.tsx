8c8103d564780ecb42b2becd5160479cb39ee5a6
Enbi81 <wotng2002@gmail.com>
2024-06-18 15:50:32 +0200

    V1.5.1

--- src/pages/WhatsNewPage.tsx
+++ src/pages/WhatsNewPage.tsx
@@ -4,6 +4,11 @@ export const WhatsNewPage = () => {
   return (
     <View>
       <ScrollView style={{ padding: 10 }}>
+        <VersionSection version={'V1.5.1'}>
+          <BulletPoint text={'Added new, scrollable widget to view the list of the schedules'} />
+          <BulletPoint text={'Fixed the current day widget'} />
+          <BulletPoint text={'Master notification switch now turns off schedule update notifications'} />
+        </VersionSection>
         <VersionSection version={'V1.5.0'}>
           <BulletPoint text={'Added Working Schedule Notifications'} />
           <BulletPoint text={'Auto refresh schedule when app is opened'} />
